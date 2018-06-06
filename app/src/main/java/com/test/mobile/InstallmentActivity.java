d64cb51f9495100287eacbe7182e35d2e635f501
DragonStar3392 <shouhyann@gmail.com>
2018-06-06 08:52:01 +0700

    update transport and install to be able to show after pressing button

--- app/src/main/java/com/test/mobile/InstallmentActivity.java
+++ app/src/main/java/com/test/mobile/InstallmentActivity.java
@@ -1,6 +1,13 @@
 package com.test.mobile;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class InstallmentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.installation);
+    }
 }
--- app/src/main/java/com/test/mobile/TransportActivity.java
+++ app/src/main/java/com/test/mobile/TransportActivity.java
@@ -1,7 +1,14 @@
 package com.test.mobile;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class TransportActivity extends Activity {
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.transport);
+    }
 }
