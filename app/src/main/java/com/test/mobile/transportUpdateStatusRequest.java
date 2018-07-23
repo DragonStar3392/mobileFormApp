861ce3c2c18a1f139baab0687cf1138334b6eb4b
DragonStar3392 <shouhyann@gmail.com>
2018-07-23 14:22:33 +0700

    onLongClick() and refresh table

---
+++ app/src/main/java/com/test/mobile/transportUpdateStatusRequest.java
@@ -0,0 +1,24 @@
+package com.test.mobile;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class transportUpdateStatusRequest extends StringRequest{
+    private static final String TRANSPORT_STATUS_UPDATE_URL = "http://10.0.2.2/updateTransportStatus.php";
+    private Map<String, String> params;
+
+    public transportUpdateStatusRequest(String id, String status, Response.Listener<String> listener) {
+        super(Method.POST, TRANSPORT_STATUS_UPDATE_URL, listener, null);
+        params = new HashMap<>();
+        params.put("ID", id);
+        params.put("status",status);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
